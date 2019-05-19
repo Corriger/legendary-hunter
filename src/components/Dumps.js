535665ef9bb22dd610169a09c6e3a12b83f3de75
Roger M <rsmoraldo2@gmail.com>
2019-05-19 12:34:30 -0400

    outlined to styled component

--- src/components/Dumps.js
+++ src/components/Dumps.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+const Dump = styled.p`
+  display: block;
+  padding: 15px;
+`
+
 class Dumps extends Component {
   render() {
     return (
       <div>
-        <p style={{
-          display: 'block',
-          padding: '15px'
-        }}>
-
-        </p>
+        <Dump>
+        </Dump>
       </div>
     )
   }

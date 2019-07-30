3fe2b7d0b5b5b5706d1f19f57bc4bb0f59cc8ac0
Roger M <rsmoraldo2@gmail.com>
2019-07-30 04:37:53 -0400

    app update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
 import NavMenu from './components/NavMenu.js'
-// import InfoKiosk from './components/InfoKiosk.js'
+import InfoKiosk from './components/InfoKiosk.js'
+import Stats from './components/Stats.js'
+import Return from './components/Return.js'
+import WeaponSelect from './components/WeaponSelect.js'
+import Dumps from './components/Dumps.js'
+import Demonstration from './components/Demonstration.js'
 import './index.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavMenu/>
+        <section className="container">
+          <NavMenu/>
+        </section>
+        // File
+        <section className="container">
+          <InfoKiosk>
+            
+          </InfoKiosk>
+        </section>
+        // Power
+        <section className="container">
+          <InfoKiosk>
+          </InfoKiosk>
+        </section>
+        // Arms
+        <section className="container">
+          <InfoKiosk>
+          </InfoKiosk>
+        </section>
+        // Guns
+        <section className="container">
+          <InfoKiosk>
+          </InfoKiosk>
+        </section>
       </div>
     )
   }

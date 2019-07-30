37768232472943ee80d60025b75dce1d3c1735f7
Roger M <rsmoraldo2@gmail.com>
2019-07-30 04:37:21 -0400

    data

---
+++ src/assets/data/Nav.js
@@ -0,0 +1,10 @@
+const navigation = [
+   {name: 'File', description: 'Defy the destiny of dark blood.'},
+   {name: 'Power', description: 'Surpass those who came before.'},
+   {name: 'Arms', description: 'Turn conquest into companionship.'},
+   {name: 'Guns', description: 'Learn the tools of the trade.'},
+   {name: 'Styles', description: 'Master all aspects of the self.'},
+   {name: 'Trigger', description: 'Unleash the true devil within.'}
+]
+
+export default

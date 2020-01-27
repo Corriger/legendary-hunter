2da1c09b53abd11ec2d2e2cf74fc0e20336a3fb2
Roger M <rsmoraldo2@gmail.com>
2020-01-27 17:28:27 -0500

    app update with components

--- src/App.js
+++ src/App.js
@@ -16,16 +16,48 @@ let fileNavItems = [
   'history',
   'outfits'
 ]
-let fileKioskItems
+let fileKioskItems = [
+  [
+    'name',
+    'd.o.b.',
+    'height',
+    'weight',
+    'blood type',
+    'species',
+    'title(s)',
+    'occupation',
+    'hobbies',
+    'likes',
+    'dislikes',
+    'alignment'
+  ],
+  [
+    'dante (née sparda)',
+    '09\/15\/1955',
+    '6\'6\"',
+    '216 lbs',
+    'AB\-',
+    'awakened cambion',
+    'legendary devil hunter, slayer, tony redgrave',
+    'paranormal mercenary, private investigator',
+    'none',
+    'his wife, the occasional fight, pizza, strawberry sundae',
+    'olives, smoking, blind ambition',
+    'true neutral'
+  ]
+
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <InfoKiosk
-          bgImg={ fileBG }
-          title='file'
-          items={fileNavItems}/>
+            bgImg={ fileBG }
+            title='file'
+            items={ fileNavItems }
+            information={ fileKioskItems }
+            />
       </div>
     )
   }

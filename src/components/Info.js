d8f3479e84171c450dbbbeecf47ac2c44b55f47f
Roger M <rsmoraldo2@gmail.com>
2020-06-04 13:36:57 -0400

    fixed merge conflict

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,36 +1,5 @@
 import React, { Component } from 'react'
-<<<<<<< HEAD:src/components/Info.js
 import { List, ListItem, SupTitle, Desc } from './styledCSS.js'
-=======
-import styled from 'styled-components'
-
-const List = styled.ul`
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-`
-const ListItem = styled.li`
-  color: #FF0000;
-  display: block;
-  padding: 0 0 15px;
-  width: 50%;
-`
-const SupTitle = styled.em`
-  color: #FFF;
-  vertical-align: super;
-  font-size: 0.6666em;
-  font-weight: bold;
-  text-decoration: capitalize;
-`
-const Desc = styled.p`
-  display: block;
-  padding: 0 0 0 15px;
-  font-size: 1em;
-  text-decoration: capitalize;
-`
->>>>>>> page-file:src/components/Stats.js
 
 class Stats extends Component {
   render() {

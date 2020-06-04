ad3a8634ee511fd8e3d3c90600eccf6b02d5a8c1
Roger M <rsmoraldo2@gmail.com>
2020-06-04 13:15:05 -0400

    moved styled-components to single file

--- src/components/InfoKiosk.js
+++ src/components/InfoKiosk.js
@@ -1,46 +1,6 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import pageBG from './../assets/img/file-bg.jpg'
-
-const Title = styled.h1`
-  display: block;
-  width: 100%;
-  font-size: 4em;
-  text-align: right;
-  color: #FFF;
-  &:after{
-    position: absolute;
-    content: "";
-    top: 0;
-    left: 0;
-    width: 100%;
-    padding: 0 0 90px;
-    border-bottom: 15px solid #5E0000;
-    z-index: 0;
-  }
-`
-const Kiosk = styled.ul`
-  margin: 0 30px;
-  width: calc(47% - 60px);
-  height: 568px;
-  background: #5E0000;
-`
-const Selection = styled.li`
-  cursor: pointer;
-  color: #FFF;
-  font-size: 2.4em;
-  width: 40%;
-  padding: 7.5px 7.5px 7.5px 30px;
-  text-transform: capitalize;
-  text-shadow: 0px 0px 1px #000, 0px 0px 1px #000, 0px 0px 3px #000, 0px 0px 5px #000;
-  &:hover{
-    color: #FFB800;
-  }
-`
-const Repo = styled(Kiosk)`
-  font-size: 1em;
-  overflow-y: auto;
-`
+import { PageTitle, Kiosk, Selection, Repo } from './styledCSS.js'
+import fileBG from './../assets/img/file-bg.jpg'
 
 class InfoKiosk extends Component {
     render() {

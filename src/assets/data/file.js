a13d6d8d40c35db37a629fe43064d4d459b74dc3
Roger M <rsmoraldo2@gmail.com>
2020-06-06 12:01:14 -0400

    data variables

---
+++ src/assets/data/file.js
@@ -0,0 +1,31 @@
+const fileNavItems = [
+    'statistics',
+    'appearance',
+    'personality',
+    'relationships',
+    'history',
+    'outfits'
+]
+
+const statistics = [
+    ['name', 'dante'],
+    ['title(s)', 'legendary devil hunter, slayer, redgrave'],
+    ['d.o.b.', '09\/15\/1955'],
+    ['occupation', 'paranormal mercenary, private investigator'],
+    ['height', '6\'6\"'],
+    ['hobbies', 'none'],
+    ['weight', '216 lbs'],
+    ['likes', 'his wife, pizza, strawberry sundaes'],
+    ['blood type', 'AB-'],
+    ['dislikes', 'olives, smoking, ambition'],
+    ['species', 'awakened cambion'],
+    ['alignment', 'true neutral']
+]
+
+const appearance = [
+    ['physiological','placeholder text'],
+    ['way of dress','placeholder number 2'],
+    ['demeanor', 'placeholder number 3']
+]
+
+export { fileNavItems, statistics, appearance }

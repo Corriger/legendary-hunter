04146da47a06ca4df88ecfa9dcc8fe2439948ec2
Roger M <rsmoraldo2@gmail.com>
2020-06-10 15:59:48 -0400

    inherit function prop??

--- src/components/InfoKiosk.js
+++ src/components/InfoKiosk.js
@@ -4,6 +4,9 @@ import Return from './Return.js'
 import { PageTitle, Kiosk, Selection, Repo } from './styledCSS.js'
 
 class InfoKiosk extends Component {
+  inheritFunction(){
+    return this.props.inherit
+  }
     render() {
       return (
         <div
@@ -19,8 +22,8 @@ class InfoKiosk extends Component {
         }}>
           <PageTitle>{this.props.title}</PageTitle>
           <Kiosk>
-            { this.props.items.map((item) => {
-              return <Selection>{ item }</Selection>
+            { this.props.items.map((item, i) => {
+              return <Selection onClick={ this.inherit } key={i}>{ item }</Selection>
             }) }
           </Kiosk>
           <Repo>

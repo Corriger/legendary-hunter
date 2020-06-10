da90e0945a718b4c388e9986439932ca3a7c268e
Roger M <rsmoraldo2@gmail.com>
2020-06-10 15:58:31 -0400

    attempt at click event
    need to figure out how to make that function and state accessible to all the children components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react'
 import NavMenu from './components/NavMenu.js'
-import { fileNavItems, statistics, appearance } from './assets/data/file.js'
+import * as File from './assets/data/file.js'
 import InfoKiosk from './components/InfoKiosk.js'
 import fileBG from './assets/img/file-bg.jpg'
 import './index.css'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.textEventHandler = this.textEventHandler.bind(this)
+    this.state = {
+      data: File,
+      focusData: ' ',
+      currentData: ' '
+    }
+  }
+
+  textEventHandler(e){
+    e = e.target.innerText
+    console.log(e)
+    return this.setState({
+      currentData: this.state.data.e
+    })
+  }
+
   render() {
+    const inFo = this.state.data
+    let infoCurrent = this.state.currentData
+    console.log(Object.keys(inFo))
+    console.log(infoCurrent)
     return (
       <div className="App">
           <InfoKiosk
+            inherit={this.textEventHandle}
             bg={fileBG}
             title='File'
-            items={fileNavItems}
-            information={statistics}
+            items={inFo.fileNavItems}
+            information={Object.keys(infoCurrent)}
           />
       </div>
     )

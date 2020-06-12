8a935993bb7ca007678e613496a9bdba85f1cb09
Roger M <rsmoraldo2@gmail.com>
2020-06-12 16:26:55 -0400

    added new variables for data

--- src/assets/data/file.js
+++ src/assets/data/file.js
@@ -1,21 +1,50 @@
 const statistics = [
-    ['name', 'dante'],
-    ['title(s)', 'legendary devil hunter, slayer'],
-    ['d.o.b.', '09/15/1955'],
-    ['occupation', 'paranormal mercenary, private investigator'],
-    ['height', `6'6"`],
-    ['hobbies', 'none'],
-    ['weight', '216 lbs'],
-    ['likes', 'his wife, pizza, strawberry sundaes'],
-    ['blood type', 'AB-'],
-    ['dislikes', 'olives, smoking, Extremism, Overambition'],
-    ['species', 'awakened cambion']
+    [`name`, `dante`],
+    [`title(s)`, `legendary devil hunter, slayer`],
+    [`d.o.b.`, `09/15/1955`],
+    [`occupation`, `paranormal mercenary, private investigator`],
+    [`height`, `6'6"`],
+    [`hobbies`, `none`],
+    [`weight`, `216 lbs`],
+    [`likes`, `his wife, pizza, strawberry sundaes`],
+    [`blood type`, `AB-`],
+    [`dislikes`, `olives, smoking, Extremism, Overambition`],
+    [`species`, `awakened cambion`]
 ]
 
 const appearance = [
-    ['physiological','placeholder text'],
-    ['way of dress','placeholder number 2'],
-    ['demeanor', 'placeholder number 3']
+    [`physiological`,`placeholder text`],
+    [`way of dress`,`placeholder number 2`],
+    [`demeanor`, `placeholder number 3`]
 ]
 
-export { statistics, appearance }
+const personality = [
+    [`attitude`,`placeholder text`],
+    [`personality`,`placeholder number 2`],
+    [`duress`, `placeholder number 3`]
+]
+
+const relationships = [
+    [`familial`,`placeholder text`],
+    [`acquaintances`,`placeholder number 2`],
+    [`adversaries`, `placeholder number 3`]
+]
+
+const history = [
+    [`the cursed child`,`placeholder text`],
+    [`tough crowd`,`placeholder number 2`],
+    [`perfect harmony`, `placeholder number 3`],
+    [`alice in no man's land`, `placeholder number 3`],
+    [`water of the womb`, `placeholder number 3`],
+    [`in the name of the father`, `placeholder number 3`],
+    [`blood of the covenant`, `placeholder number 3`],
+    [`new challenger`, `placeholder number 3`],
+    [`eternal darkness`, `placeholder number 3`],
+    [`reason beyond`, `placeholder number 3`],
+    [`debt consolidation`, `placeholder number 3`],
+    [`a machine of beasts`, `placeholder number 3`],
+    [`faith, fortitude, fruitition`, `placeholder number 3`],
+    [`conceptual labrynth`, `placeholder number 3`]
+]
+
+export { statistics, appearance, personality, relationships, history }

f6a64b5ae2602a85d0c0d76a549103764cfd0af7
Roger M <rsmoraldo2@gmail.com>
2020-06-21 12:27:54 -0400

    raw html

--- src/components/WeaponSelect.js
+++ src/components/WeaponSelect.js
@@ -3,12 +3,40 @@ import PropTypes from 'prop-types'
 import Return from './Return.js'
 
 class InfoKiosk extends Component {
-    render() {
-      return (
-        <div>
-          <Return return={this.props.return}/>
-        </div>
-      )
+  render() {
+    return (
+      <div>
+        <article>
+          <p></p>
+        </article>
+        <ul>
+          <li class='prev'></li>
+          <li>
+            <img class='weapon' src='' alt='weapon 1' />
+            <div>
+              <strong>Test 1</strong>
+              <em>The Testening</em>
+            </div>
+          </li>
+          <li>
+            <img class='weapon' src='' alt='weapon 2' />
+            <div>
+              <strong>Test 2</strong>
+              <em>Akuma No Testo</em>
+            </div>
+          </li>
+          <li>
+            <img class='weapon' src='' alt='weapon 3' />
+            <div>
+              <strong>Test 3</strong>
+              <em>Tester's Awakening</em>
+            </div>
+          </li>
+          <li class='next'></li>
+        </ul>
+        <Return return={this.props.return} />
+      </div>
+    )
   }
 }
 
